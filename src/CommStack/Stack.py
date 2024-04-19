8441cde153ad1029ab09f22e22901fee19d845cd
Sam <samuelgardner101@gmail.com>
2024-04-19 13:30:37 +0100

    Access layers properly

--- src/CommStack/Stack.py
+++ src/CommStack/Stack.py
@@ -6,54 +6,38 @@ from threading import Thread
 
 
 class Stack:
-    _level0: Level0
-    _level1: Level1
-    _level2: Level2
-    _levelD: LevelD
+    level0: Level0
+    level1: Level1
+    level2: Level2
+    levelD: LevelD
 
     def __init__(self):
-        self._level0 = self._level1 = self._level2 = self._levelD = None
+        self.level0 = self.level1 = self.level2 = self.levelD = None
         Thread(target=self._init_level_0).start()
         Thread(target=self._init_level_1).start()
         Thread(target=self._init_level_2).start()
         Thread(target=self._init_level_D).start()
 
     def _init_level_0(self):
-        self._level0 = Level0()
+        self.level0 = Level0()
 
     def _init_level_1(self):
-        while not self._level0: pass
-        self._level1 = Level1(self._level0)
+        while not self.level0: pass
+        self.level1 = Level1(self.level0)
 
     def _init_level_2(self):
-        while not self._level1: pass
-        self._level2 = Level2(self._level1)
+        while not self.level1: pass
+        self.level2 = Level2(self.level1)
 
     def _init_level_D(self):
-        while not self._level0: pass
-        self._levelD = LevelD(self._level0)
+        while not self.level0: pass
+        self.levelD = LevelD(self.level0)
 
     def handle_create_route(self):
-        self._level2.create_route()
+        self.level2.create_route()
 
     def store_file(self, file_directory) -> None:
-        self._level2._level1._level0.put(file_directory)
+        self.level0.put(file_directory)
 
     def retrieve_file(self, file_name) -> None:
-        self._level2._level1._level0.get(file_name)
-
-    @property
-    def levelD(self):
-        return self._levelD
-
-    @property
-    def level2(self):
-        return self._level2
-
-    @property
-    def level1(self):
-        return self._level2._level1
-
-    @property
-    def level0(self):
-        return self._level2._level1._level0
+        self.level0.get(file_name)
