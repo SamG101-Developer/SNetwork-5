b4148747ca4b0a9d59692930a0b08be5272949b0
Sam <samuelgardner101@gmail.com>
2024-04-19 13:00:17 +0100

    Python < 3.12 compat

--- src/Utils/Types.py
+++ src/Utils/Types.py
@@ -1,31 +1,28 @@
 from __future__ import annotations
 from typing import Optional as _Optional
-import sys
 
+Bool = bool
+Bytes = bytes
+Dict = dict
+Float = float
+Int = int
+List = list
+Optional = _Optional
+Set = set
+Str = str
+Tuple = tuple
+Type = type
+Json = Dict
 
-if sys.version_info < (3, 12):
-    Bool = bool
-    Bytes = bytes
-    Dict = dict
-    Float = float
-    Int = int
-    List = list
-    Optional = _Optional
-    Set = set
-    Str = str
-    Tuple = tuple
-    Type = type
-    Json = Dict
-else:
-    type Bool = bool
-    type Bytes = bytes
-    type Dict[K, V] = dict[K, V]
-    type Float = float
-    type Int = int
-    type List[T] = list[T]
-    type Optional[T] = _Optional[T]
-    type Set[T] = set[T]
-    type Str = str
-    type Tuple[*Ts] = tuple[Ts]
-    type Type[T] = type[T]
-    type Json = Dict[Str, Str | Int | Bool | List[Json] | Json]
+# type Bool = bool
+# type Bytes = bytes
+# type Dict[K, V] = dict[K, V]
+# type Float = float
+# type Int = int
+# type List[T] = list[T]
+# type Optional[T] = _Optional[T]
+# type Set[T] = set[T]
+# type Str = str
+# type Tuple[*Ts] = tuple[Ts]
+# type Type[T] = type[T]
+# type Json = Dict[Str, Str | Int | Bool | List[Json] | Json]
