a841bab0a5a4b6dfe2936e2e42be233b17151db9
Sam <samuelgardner101@gmail.com>
2024-04-19 15:56:26 +0100

    Bug joining network

--- src/CommStack/LevelD.py
+++ src/CommStack/LevelD.py
@@ -90,6 +90,8 @@ class LevelD(LevelN):
         # Add the node to the DHT.
         for ip in node_ip_addresses:
             ip = IPv4Address(bytes.fromhex(ip))
+            if ip == my_address():
+                continue
             logging.debug(f"Joining network at {ip}...")
             if self._level0.join(ip): break
 
