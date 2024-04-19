42fa4aa9ed0ce29e400e3147b26d831a585fa8b0
Sam <samuelgardner101@gmail.com>
2024-04-19 13:05:44 +0100

    Make crypt folder

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from PyQt6.QtWidgets import QApplication
-import logging, sys
+import logging, os, sys
 
 from src.Gui.MainWindow import MainWindow
 
 
 def main() -> None:
+    if not os.path.exists("_crypt"):
+        os.mkdir("_crypt")
+
     logging.basicConfig(level=logging.DEBUG)
     sys.excepthook = lambda *args: sys.__excepthook__(*args)
     app = QApplication(sys.argv)
