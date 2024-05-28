4876908559c092977f48a2deb543cbf35b38c8f8
Sam Gardner <samuelgardner101@gmail.com>
2024-05-28 14:02:02 +0100

    Safe JSON to handle loading/dumping errors

---
+++ src/SNetwork/Utils/Json.py
@@ -0,0 +1,21 @@
+import json
+
+from SNetwork.Utils.Types import Bytes, Str, Json, Callable, Optional
+
+
+class SafeJson:
+    @staticmethod
+    def loads(string: Bytes | Str, error_handler: Optional[Callable[[...], None]] = None) -> Json:
+        try:
+            return json.loads(string)
+        except json.JSONDecodeError:
+            error_handler and error_handler()
+            return {}
+
+    @staticmethod
+    def dumps(data: Json, error_handler: Optional[Callable[[...], None]] = None) -> Str:
+        try:
+            return json.dumps(data)
+        except TypeError:
+            error_handler and error_handler()
+            return "{}"
