996e278f6d231d1de6230cefe8d114823afa597a
Sam Gardner <samuelgardner101@gmail.com>
2024-05-28 14:00:37 +0100

    Add the `Callable` type

--- src/SNetwork/Utils/Types.py
+++ src/SNetwork/Utils/Types.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-from typing import Optional as _Optional
+from typing import Optional as _Optional, Callable as _Callable
 
 type Bool = bool
 type Bytes = bytes
+type Callable = _Callable
 type Dict[K, V] = dict[K, V]
 type Float = float
 type Int = int
@@ -14,5 +15,5 @@ type Tuple[*Ts] = tuple[Ts]
 type Type[T] = type[T]
 type Json = Dict[Str, Str | Int | Bool | List[Json] | Json]
 
-
-__all__ = ["Bool", "Bytes", "Dict", "Float", "Int", "List", "Optional", "Set", "Str", "Tuple", "Type", "Json"]
+__all__ = [
+    "Bool", "Bytes", "Callable", "Dict", "Float", "Int", "List", "Optional", "Set", "Str", "Tuple", "Type", "Json"]
