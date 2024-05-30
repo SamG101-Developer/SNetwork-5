927542639a033edf07edaa0317e16f7548e59bdd
Sam Gardner <samuelgardner101@gmail.com>
2024-05-30 23:41:44 +0100

    Mitigate bad json with "retry" system (WIP)

--- src/SNetwork/CommStack2/LayerN.py
+++ src/SNetwork/CommStack2/LayerN.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 
+import secrets
+import time
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from ipaddress import IPv6Address
 from socket import socket as Socket, AF_INET6, SOCK_DGRAM
+from threading import Thread, Lock
 
-from SNetwork.Crypt.AsymmetricKeys import SecKey
-from SNetwork.Utils.Types import Bytes, Json, Int, Optional
+from SNetwork.Crypt.AsymmetricKeys import PubKey, SecKey
+from SNetwork.Utils.Types import Bytes, Json, Int, Optional, Dict, Float, Tuple
+from SNetwork.Utils.Json import SafeJson
+from SNetwork.Config import CONNECTION_TIMEOUT
 
 
 @dataclass(kw_only=True)
@@ -31,7 +36,7 @@ class Connection:
     token: Bytes
     state: LayerNProtocol
     challenge: Optional[Bytes]
-    ephemeral_public_key: Optional[Bytes]
+    ephemeral_public_key: Optional[PubKey]
     ephemeral_secret_key: Optional[SecKey]
     e2e_primary_key: Optional[Bytes]
 
@@ -51,6 +56,7 @@ class LayerN(ABC):
 
     Attributes:
         _socket: The socket object used to send and receive data.
+        _message_map: A dictionary used to store recently sent messages.
 
     Methods:
         _listen: The method that listens for incoming data.
@@ -60,14 +66,23 @@ class LayerN(ABC):
     """
 
     _socket: Socket
+    _message_map: Dict[Bytes, Tuple[Float, Json]]
+    _message_map_lock: Lock
 
-    def __init__(self, socket_type: int = SOCK_DGRAM):
+    def __init__(self, socket_type: Int = SOCK_DGRAM):
         """
         The constructor for the LayerN class. This method creates a new socket object, which is used to send and receive
         data. The socket type is defined by the socket_type parameter, which defaults to SOCK_DGRAM. The only time UDP
         isn't used is for the Layer1 proxy socket, which listens for TCP connections, to proxy the data out.
         """
+
+        # Initialize the socket and message map.
         self._socket = Socket(AF_INET6, socket_type)
+        self._message_map = {}
+        self._message_map_lock = Lock()
+
+        # Start the message map cleaner thread.
+        Thread(target=self._clean_message_map).start()
 
     @abstractmethod
     def _listen(self) -> None:
@@ -102,6 +117,31 @@ class LayerN(ABC):
         correct port, and to identify the layer in the Communication Stack. It is defined as a property to allow
         abstract methods to use the port no matter the implementation.
         """
+
+    def _prep_data(self, connection: Connection, data: Json) -> Bytes:
+        """
+        This method is used to prepare the data to be sent to a connection. The data has the connection stored under the
+        "token" key, and a random message ID added to, for re-sending malformed messages. The data is then dumped to
+        JSON, and converted to bytes and returned.
+        """
+
+        data["token"] = connection.token
+        data["id"] = secrets.token_bytes(16)
+        self._message_map[data["id"]] = (time.time(), data)
+        return SafeJson.dumps(data)
+
+    def _clean_message_map(self) -> None:
+        """
+        This method is used to clean the message map of old messages. The message map is a dictionary that stores the
+        message ID and the time it was sent. If the message is older than the CONNECTION_TIMEOUT, it is removed from the
+        dictionary.
+        """
+
+        while True:
+            time.sleep(CONNECTION_TIMEOUT)
+            with self._message_map_lock:
+                timeout_check = lambda x: time.time() - x[0] < CONNECTION_TIMEOUT
+                self._message_map = {k: v for k, v in self._message_map.items() if timeout_check(v)}
 
     def __del__(self):
         """
