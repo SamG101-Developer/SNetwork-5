896907c982448e3e2d99eefa9873bb5a5e6aecfe
Sam Gardner <samuelgardner101@gmail.com>
2024-05-30 23:40:11 +0100

    Selectable dictionaries to combine all input streams to single outgoing connection

--- src/SNetwork/CommStack2/Layer1.py
+++ src/SNetwork/CommStack2/Layer1.py
@@ -1,110 +1,96 @@
+import select
+from enum import Enum
 from ipaddress import IPv6Address
-from socket import socket as RawSocket, SOCK_STREAM
+from socket import socket as Socket, SOCK_STREAM
 from threading import Thread
 
+from SNetwork.Config import LAYER_1_PORT
 from SNetwork.CommStack2.LayerN import LayerN, LayerNProtocol, Connection
-from SNetwork.Config import LAYER_0_PORT, LOOPBACK_IPV6
-from SNetwork.Utils.Types import Int, Json, Dict, Str
+from SNetwork.CommStack2.Layer2 import Layer2
+from SNetwork.Utils.Types import Int, Json
+from SNetwork.Utils.HttpParser import HttpParser
+from SNetwork.Utils.SelectableDict import SelectableDict, Selectable
 
 
-class Layer1Protocol(LayerNProtocol):
-    InitConnection = 0
-    ConnectionEstablished = 1
-
-
-class SelectableDict[K, V]:
-    """
-    The SelectableDict wraps a dictionary class with an IO-like access to get values associated with possibly existing
-    keys. In the same way that socket.recv() will wait until data is available, this class will wait until a key is
-    available in the dictionary. This allows it to be used in "select.select()" calls, with other buffers like sockets.
-    """
-
-    _dict: Dict[K, V]
-
-    def __init__(self):
-        self._dict = {}
-
-    def __getitem__(self, key: K) -> V:
-        while key not in self._dict:
-            pass
-        return self._dict[key]
-
-    def __setitem__(self, key: K, value: V) -> None:
-        self._dict[key] = value
-
-    def __delitem__(self, key: K) -> None:
-        del self._dict[key]
-
-    def __contains__(self, key: K) -> bool:
-        return key in self._dict
-
-    def __len__(self) -> Int:
-        return len(self._dict)
+class Layer1Protocol(LayerNProtocol, Enum):
+    ...
 
 
 class Layer1(LayerN):
-    _socket_map: SelectableDict[Int, RawSocket]
+    _layer2: Layer2
+    _selectable_dict: SelectableDict[Int, Json]
 
-    def __init__(self):
-        # The Layer0 socket must be a TCP socket, as it is used to intercept messages as a proxy for HTTPS connections.
+    def __init__(self, layer2: Layer2):
         super().__init__(SOCK_STREAM)
 
+        self._layer2 = layer2
+        self._selectable_dict = SelectableDict[Int, Json]()
+
     def _listen(self) -> None:
-        # The local interceptor socket is used to intercept messages as a proxy. It operates on a different port to
-        # standard Layer1 data transfer, to separate data from other routes that use this node as a route node.
-        self._socket.bind((LOOPBACK_IPV6, LAYER_0_PORT))
-        self._socket.listen(20)
-
-        #
         while True:
-            client_socket, address = self._socket.accept()
-            Thread(target=self._handle_command, args=(address, client_socket)).start()
+            client_socket, _ = self._socket.accept()
+            Thread(target=self._handle_client, args=(client_socket, )).start()
 
-    def _unique_id(self) -> Int:
-        return len(self._socket_map) + 1
-
-    def _handle_command(self, address: IPv6Address, client_socket: RawSocket) -> None:
-        # Handle the CONNECT request from the client.
+    def _handle_client(self, client_socket: Socket) -> None:
+        # Receive the CONNECT request, and extract the host.
         data = client_socket.recv(1024)
-        lines = data.split(b"\r\n")
-        _, target_host, _ = lines[0].split(b" ")
+        host = HttpParser(data).headers[b"Host"].decode()
 
-        # Send a 200 OK response to the client
+        # Get the target key from the dictionary.
+        unique_id = len(self._selectable_dict) + 1
+        # todo: forward message to entry node with "unique_id" attached [layer 2]
+
+        # Get the unfilled Selectable and send a connection established response.
+        target_getter = self._selectable_dict[unique_id]
         client_socket.sendall(b"HTTP/1.1 200 Connection Established\r\n\r\n")
+
+        # Set the sockets to non-blocking mode.
+        target_getter.setblocking(False)
         client_socket.setblocking(False)
 
-        # Handle data transfer between the client, and the target
-        Thread(target=self._manage_data_transfer, args=(client_socket, target_host)).start()
+        # Handle data transfer between the client, and the target.
+        Thread(target=self._handle_data_transfer, args=(unique_id, client_socket, target_getter)).start()
+
+    def _handle_data_transfer(self, unique_id: Int, client_socket: Socket, target_getter: Selectable[Int]) -> None:
+        try:
+            # Store the sockets in a list, for the selection.
+            sockets = [client_socket, target_getter]
+            while True:
+
+                # Get the readable and errored sockets.
+                readable, _, errored = select.select(sockets, [], sockets)
+
+                # Receive data from the readable sockets, and send it to the other socket.
+                for sock in readable:
+                    data = sock.recv(4096)
+                    if not data:
+                        break
+                    if sock is client_socket:
+                        target_getter.set_value(data)
+                    else:
+                        client_socket.sendall(data)
+
+                # Remove and close any errored sockets (prevents trying to read from a closed socket).
+                for sock in errored:
+                    sockets.remove(sock)
+                    sock.close()
+
+        except ConnectionResetError:
+            # If there is an error, close the sockets and exit the thread. This would be done anyway, so the code is in
+            # the "finally" block.
+            ...
+
+        finally:
+            # Close the sockets.
+            client_socket.close()
+            target_getter.close()
+
+    def _handle_command(self, address: IPv6Address, request: Json) -> None:
+        pass
 
     def _send(self, connection: Connection, data: Json) -> None:
         pass
 
     @property
     def _port(self) -> Int:
-        return LAYER_0_PORT
-
-    def _manage_data_transfer(self, client_socket: RawSocket, target_host: Str) -> None:
-        try:
-            sockets = [client_socket, target_socket]
-            while True:
-                readable, _, exceptional = select.select(sockets, [], sockets)
-                for sock in readable:
-                    data = sock.recv(4096)
-                    if not data: break
-                    if sock is client_socket:
-                        print(f"Data received from client [{client_socket.getpeername()[0]}]: {data}")
-                        target_socket.sendall(data)
-                    else:
-                        print(f"Data received from target [{target_socket.getpeername()[0]}]: {data}")
-                        client_socket.sendall(data)
-
-                for sock in exceptional:
-                    sockets.remove(sock)
-                    sock.close()
-
-        except ConnectionResetError:
-            print("Connection reset by client")
-
-        finally:
-            client_socket.close()
-            target_socket.close()
+        return LAYER_1_PORT
