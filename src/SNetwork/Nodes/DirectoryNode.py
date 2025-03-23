5b053a15cb1897bbdf96dd68072570f8adcf76e4
Sam Gardner <samuelgardner101@gmail.com>
2025-03-23 17:45:23 +0000

    Simplify node structuring

--- src/SNetwork/Nodes/DirectoryNode.py
+++ src/SNetwork/Nodes/DirectoryNode.py
@@ -1,48 +1,37 @@
-import json
-
 from SNetwork.CommunicationStack.CommunicationStack import CommunicationStack
 from SNetwork.CommunicationStack.Layers_1stParty.LayerD import LayerD
-from SNetwork.Config import DIRECTORY_SERVICE_PRIVATE_FILE
-from SNetwork.Managers.KeyManager import KeyManager, KeyStoreData
+from SNetwork.Managers.KeyManager import KeyManager
 from SNetwork.Nodes.Node import Node
 from SNetwork.QuantumCrypto.Keys import AsymmetricKeyPair
-from SNetwork.Utils.Logger import isolated_logger, LoggerHandlers
 from SNetwork.Utils.Types import Bytes, Int, Str
 
 
 class DirectoryNode(Node):
-    _stack: CommunicationStack
-    _boot: LayerD
-    _info: KeyStoreData
     _name: Str
 
-    def __init__(self, name, hashed_username: Bytes, hashed_password: Bytes, port: Int, identifier: Bytes, static_key_pair: AsymmetricKeyPair) -> None:
+    def __init__(
+            self, name, hashed_username: Bytes, hashed_password: Bytes, port: Int, identifier: Bytes,
+            static_key_pair: AsymmetricKeyPair) -> None:
         self._name = name
 
         # Create the communication stack, and the bootstrapper layer.
         self._stack = CommunicationStack(hashed_username, port)
-        self._boot = LayerD(self._stack, self._stack._socket_ln, True, identifier, static_key_pair)
 
         # Check if the node has been registered before.
         has_info = KeyManager.has_info(hashed_username)
         if not has_info:
-            self._boot_sequence(hashed_username, hashed_password)
+            self._boot_sequence(hashed_username, hashed_password, identifier, static_key_pair)
 
-        # Save the information of the node and start the communication stack.
+        # Save the information of the node.
         self._info = KeyManager.get_info(hashed_username)
+
+        # Start the special directory layer communications and start the communication stack.
         self._stack.start(self._info)
+        self._boot = LayerD(self._stack, self._stack._socket_ln, True, self._info.identifier, self._info.certificate, static_key_pair)
+        self._stack._logger.info(f"Directory Node started.")
 
-    def _boot_sequence(self, hashed_username: Bytes, hashed_password: Bytes) -> None:
-        # Set the keys.
-        with open(DIRECTORY_SERVICE_PRIVATE_FILE % self._name, "rb") as file:
-            private_directory_service_entry = json.load(file)
-            logger = isolated_logger(LoggerHandlers.SYSTEM)
-
-        # Store the certificate and other information in the key store.
-        KeyManager.set_info(KeyStoreData(
-            identifier=bytes.fromhex(private_directory_service_entry["identifier"]),
-            secret_key=bytes.fromhex(private_directory_service_entry["secret_key"]),
-            public_key=bytes.fromhex(private_directory_service_entry["public_key"]),
-            certificate=None,
-            hashed_username=hashed_username,
-            hashed_password=hashed_password))
+    def _boot_sequence(
+            self, hashed_username: Bytes, hashed_password: Bytes, identifier: Bytes = None,
+            static_key_pair: AsymmetricKeyPair = None) -> None:
+        self._stack._logger.info(f"Registering directory node {identifier.hex()}.")
+        self._generate_certificate(hashed_username, hashed_password, identifier, static_key_pair)
--- src/SNetwork/Nodes/Node.py
+++ src/SNetwork/Nodes/Node.py
@@ -1,56 +1,46 @@
-import pickle
+from ipaddress import IPv6Address
 
 from SNetwork.CommunicationStack.CommunicationStack import CommunicationStack
 from SNetwork.CommunicationStack.Layers_1stParty.LayerD import LayerD
-from SNetwork.Config import PROFILE_CACHE
 from SNetwork.Managers.KeyManager import KeyManager, KeyStoreData
-from SNetwork.Utils.Files import SafeFileOpen
-from SNetwork.Utils.Types import Bytes, Int
+from SNetwork.QuantumCrypto.Certificate import X509
+from SNetwork.QuantumCrypto.Hash import Hasher, HashAlgorithm
+from SNetwork.QuantumCrypto.Keys import AsymmetricKeyPair
+from SNetwork.QuantumCrypto.QuantumSign import QuantumSign
+from SNetwork.Utils.Types import Bytes, Int, Tuple, Dict
+
+
+class NodeCache:
+    """
+    A cache containing other nodes to try to connect to. The cache is maintained in a file, and is used to keep track of
+    other nodes that have been connected to in the past. Old nodes are removed from the cache, and new nodes are added
+    to the cache by asking the directory service, or a node, for a list of known nodes.
+    """
+
+    _nodes: Dict[Bytes, Tuple[Bytes, IPv6Address, Int]]
+
+    def __init__(self) -> None:
+        self._nodes = {}
+
+    def _load_local_cache(self, identifier: Bytes) -> None:
+        pass
 
 
 class Node:
     _stack: CommunicationStack
-    _boot: LayerD
     _info: KeyStoreData
+    _boot: LayerD
+    _cache: NodeCache
 
     def __init__(self, hashed_username: Bytes, hashed_password: Bytes, port: Int) -> None:
         # Create the communication stack, and the bootstrapper layer.
         self._stack = CommunicationStack(hashed_username, port)
-        self._boot = LayerD(self._stack, self._stack._socket_ln, False)
 
-        # Check if the node has been registered before.
-        if not KeyManager.has_info(hashed_username):
-            self._boot_sequence(hashed_username, hashed_password)
-        else:
-            self._stack._logger.info(f"Node already registered.")
+        # Update the node cache with the directory service.
+        self._info = KeyManager.get_info(hashed_username)
+        self._boot = LayerD(self._stack, self._stack._socket_ln, False, self._info.identifier, self._info.certificate)
 
         # Save the information of the node and start the communication stack.
-        self._info = KeyManager.get_info(hashed_username)
         self._stack.start(self._info)
+        self._boot.request_bootstrap()
         self._stack._logger.info(f"Node started.")
-
-        # Wait for the node cache to be populated.
-        while not self._boot._node_cache: continue
-
-        # Open the node cache and load the node information.
-        with SafeFileOpen(PROFILE_CACHE % hashed_username.hex(), "rb") as file:
-            node_info = pickle.load(file)
-
-        # Try to connect to a node in the distributed hash table network.
-        for node in node_info:
-            connection = self._stack._layer4.connect(*node[:3])
-            if connection: self._stack._layer3.join_distributed_hash_table_network(connection)
-
-    def _boot_sequence(self, hashed_username: Bytes, hashed_password: Bytes) -> None:
-        # Register the node against the network.
-        self._boot.join_network()
-        while not self._boot._certificate: continue
-
-        # Store the certificate and other information in the key store.
-        KeyManager.set_info(KeyStoreData(
-            identifier=self._boot._this_identifier,
-            secret_key=self._boot._this_static_key_pair.secret_key,
-            public_key=self._boot._this_static_key_pair.public_key,
-            certificate=self._boot._certificate,
-            hashed_username=hashed_username,
-            hashed_password=hashed_password))
