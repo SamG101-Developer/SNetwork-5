2b043d425b5a03a756e818d6a01ee18b5aca7212
Sam Gardner <samuelgardner101@gmail.com>
2025-03-23 17:45:10 +0000

    Default attr values

--- src/SNetwork/QuantumCrypto/Keys.py
+++ src/SNetwork/QuantumCrypto/Keys.py
@@ -1,17 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 from SNetwork.Utils.Types import Bytes
 
 
 @dataclass(kw_only=True, frozen=True)
 class AsymmetricKeyPair:
-    public_key: Bytes
-    secret_key: Bytes
+    public_key: Bytes = field(default=b"")
+    secret_key: Bytes = field(default=b"")
 
 
 @dataclass(kw_only=True, frozen=True)
 class WrappedKeyPair:
-    encapsulated: Bytes
-    decapsulated: Bytes
+    encapsulated: Bytes = field(default=b"")
+    decapsulated: Bytes = field(default=b"")
 
 
 __all__ = ["AsymmetricKeyPair", "WrappedKeyPair"]
